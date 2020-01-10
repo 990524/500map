c6eacc71298eb654bf332da6cb6507e52f77b655
jianong <jianonghe@gmail.com>
2020-01-10 20:23:57 +0800

    基础布局，引入基础类库

---
+++ src/layout/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="layout">
+    <Menu mode="horizontal" :theme="theme" active-name="1" class="header">
+      <MenuItem name="1">
+        <Icon type="ios-paper" />
+        内容管理
+      </MenuItem>
+      <MenuItem name="2">
+        <Icon type="ios-people" />
+        用户管理
+      </MenuItem>
+      <Submenu name="3">
+        <template slot="title">
+          <Icon type="ios-stats" />
+          统计分析
+        </template>
+        <MenuGroup title="使用">
+          <MenuItem name="3-1">新增和启动</MenuItem>
+          <MenuItem name="3-2">活跃分析</MenuItem>
+          <MenuItem name="3-3">时段分析</MenuItem>
+        </MenuGroup>
+        <MenuGroup title="留存">
+          <MenuItem name="3-4">用户留存</MenuItem>
+          <MenuItem name="3-5">流失用户</MenuItem>
+        </MenuGroup>
+      </Submenu>
+      <MenuItem name="4">
+        <Icon type="ios-construct" />
+        综合设置
+      </MenuItem>
+    </Menu>
+    <main class="main">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  data () {
+    return {
+      theme: 'dark' // light dark primary(mode="horizontal")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$header_height: 60px;
+
+.layout {
+  .header {
+    height: $header_height;
+  }
+
+  .main {
+    min-height: calc(100vh - $header_height);
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '../layout/index'
+import Map from '../views/Map.vue'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.use(VueRouter)
 
@@ -8,20 +11,37 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Layout,
+    redirect: 'map',
+    children: [
+      {
+        path: 'map',
+        name: 'map',
+        component: Map
+      },
+      {
+        path: 'about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
